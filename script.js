39f7f144ad2d33b07cce7f07efedeb6fb1dd565f
Nargroth108 <138591543+Nargroth108@users.noreply.github.com>
2023-08-15 12:00:30 +0200

    Create playRound function
    This will play exactly 1 round of rps,
    and can be called inside other functions later,
    if necessary.

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-const choices = ['rock', 'paper', 'scissors'];
-const playerScore = 0
-const computerScore = 0
+let playerScore = 0
+let computerScore = 0
 
 function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];;
     return choices[Math.floor(Math.random() * choices.length)];
 };
 
 console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "Tie! Play another round"
+    } else if (playerSelection ===  'rock' && computerSelection === 'paper') {
+        computerScore++
+        return `You lost, ${computerSelection} beats ${playerSelection}!`
+    } else if (playerSelection ===  'rock' && computerSelection === 'scissors') {
+        playerScore++
+        return `You won, ${playerSelection} beats ${computerSelection}!`
+    } else if (playerSelection ===  'paper' && computerSelection === 'rock') {
+        playerScore++
+        return `You won, ${playerSelection} beats ${computerSelection}!`
+    } else if (playerSelection ===  'paper' && computerSelection === 'scissors') {
+        computerScore++
+        return `You lost, ${computerSelection} beats ${playerSelection}!`
+    } else if (playerSelection ===  'scissors' && computerSelection === 'rock') {
+        computerScore++
+        return `You lost, ${computerSelection} beats ${playerSelection}!`
+    } else if (playerSelection ===  'scissors' && computerSelection === 'paper') {
+        playerScore++
+        return `You won, ${playerSelection} beats ${computerSelection}!`
+    } else {
+        return "Typo?"
+    }
+}
+const playerSelection = prompt('type').toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
 
